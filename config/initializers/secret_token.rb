ad9cc6955a0cd4bcc655c8c2ff034d490b947f96
Tilo Sloboda <tilo.sloboda@gmail.com>
2011-04-11 14:34:09 -0700

    initial submit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MailFormExampleWithRecaptcha::Application.config.secret_token = '2d2b2d4ad836d5d119bb042e6d940e36ec71f7758a3fbd4806e6081cc2e5192e2b2a802a34ce293313711a43e1dd1805db2365472bcabe50e323b5c9cc13f733'
