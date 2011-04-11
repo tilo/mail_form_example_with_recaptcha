52239e00245fb25beba3ddbfe8cfcd007bc646ac
Tilo Sloboda <tilo.sloboda@gmail.com>
2011-04-11 15:44:33 -0700

    added DRY flash helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+  #DRY flash messages
+  def flash_message
+    messages = ""
+    [:notice, :info, :warning, :error, :alert].each do|type|
+      if flash[type]
+        messages= "<div id=\"#{type}\">#{flash[type]}</div>".html_safe
+      end
+    end
+    messages
+  end
+
 end
